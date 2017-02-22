0f39029acbe818bb354906343b279db216e1ded2
Onur Akpolat <onur.akpolat@gmail.com>
2017-02-22 23:54:16 +0100

    add env setup

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractStyles = new ExtractTextPlugin('app.css');
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    app: './App.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /App\.css/,
+        loader: extractStyles.extract({
+          loader: 'css-loader?importLoaders=1!postcss-loader',
+        }),
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: ['url-loader'],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/assets'),
+    filename: '[name].bundle.js',
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+  },
+  plugins: [
+    extractStyles
+  ],
+};
